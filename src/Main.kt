c64d924521914805316a683b736c3ac11f0a27e4
Trent Schafer <trent.schafer@gmail.com>
2017-05-22 13:38:07 -0400

    Add Java print call Demonstrate printing both using the Kotlin standard library and also the familiar Java library

--- src/Main.kt
+++ src/Main.kt
@@ -5,5 +5,9 @@
 package my.hello.world
 
 fun main(args: Array<String>){
+    //Print from the kotlin standard library
+    //See here: https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.io/println.html
     println("Hello, World!")
+    //..or using the familiar Java API call
+    System.out.println("Hello, World! (for the second time)")
 }
